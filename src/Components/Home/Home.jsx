260441bb75e153ad4b7512639e9629fbd46f62ad
Rami Abdou <ra494@cornell.edu>
2019-11-27 11:24:36 -0500

    Refactored the home page.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,23 +1,24 @@
-// React Library
+/**
+ * @fileoverview The home page which includes a graphic design of URMs coding,
+ * as well as links to both the About and Join pages.
+ */
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// CSS Files
 import '../../main.css';
 import './Home.css';
 
-// Components
 import { DesktopAndTablet, Mobile } from '../Util/Breakpoints';
 import withScreenSize from '../HOC/ScreenSize';
 
-// Images
 const girl = require('../../assets/girl.png');
 
 const HomeButton = (props) => {
   return (
     <Link to={props.link} className="noDecoration colorCharcoal"
       onClick={() => props.updateCurrentPage(props.pageTitle)}>
-      <div className={`homeButton marginRight15px fitWidth pointer textAlignCenter
+      <div className={`homeButton marginRight15px fitWidth textAlignCenter
         ${props.link === '/about' ? 'aboutButton' : 'listservButton'}
         ${props.breakpoint === 'D' ? 'fontSize14px' : 'fontSize12px'}`}>
         {props.buttonTitle}
@@ -35,17 +36,18 @@ const Home = (props) => {
     M: 'girlHeight-sm marginTop15px'
   }[breakpoint]
 
-  let purposeTextClass = props.breakpoint === 'M' ? 'fontSize14px textAlignCenter' : 'fontSize16px';
+  let purposeText = breakpoint === 'M' ? 'fontSize14px textAlignCenter' : 'fontSize16px';
 
   return (
     <div className="marginTop25px displayFlex flexAlignCenter">
       <div className={`${breakpoint === 'M' ? 'marginAuto' : 'width60P'}`}>
-        <div className={`width75P marginAuto
-          ${breakpoint === 'M' ? 'flexColumnAlignCenter' : null}`}>
+        <div className={`width75P marginAuto ${breakpoint === 'M' ? 'flexColumnAlignCenter' : ''}`}>
           <div className={`fontFamilyRalewayB colorCharcoal
-            ${breakpoint === 'M' ? 'fontSize20px textAlignCenter' : 'fontSize30px'}`}>Underrepresented Minorities in Computing</div>
+            ${breakpoint === 'M' ? 'fontSize20px textAlignCenter' : 'fontSize30px'}`}>
+            Underrepresented Minorities in Computing
+          </div>
 
-          <div className={`${purposeTextClass} marginTop25px fontFamilyRaleway colorCharcoal`}>
+          <div className={`${purposeText} marginTop25px fontFamilyRaleway colorCharcoal`}>
             The purpose of Underrepresented Minorities in Computing is to promote
             diversity within the computing fields and foster an environment that
             empowers underrepresented minorities with technological aspirations
