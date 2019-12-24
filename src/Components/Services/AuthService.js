cfa1e6e973162ab83caf3b8de79ecccd660d91f6
Rami Abdou <ra494@cornell.edu>
2019-12-23 17:28:09 -0800

    Changed server URL.

--- src/Components/Services/AuthService.js
+++ src/Components/Services/AuthService.js
@@ -39,7 +39,7 @@ export default class AuthService {
 
       const options = {
         method: 'post',
-        url: `http://127.0.0.1:8080/google/auth`,
+        url: serverURL,
         data: profileAndToken,
         headers: {
           'Content-Type': 'application/json'
