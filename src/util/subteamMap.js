5533037272cd89c45c85474b491fcddc7bc9892d
Dezia10 <90987580+Dezia10@users.noreply.github.com>
2022-03-27 15:07:07 -0400

    updated subteammap

--- src/util/subteamMap.js
+++ src/util/subteamMap.js
@@ -9,6 +9,7 @@ const subteamMap = new Map([['Co-President', 'Presidents'],
 ['Co-Corporate Chair', 'Corporate'], ['Operations Chair', 'Operations'],
 ['Secretary', 'Secretary'], ['Co-Mentorship Chair', 'Mentorship'],
 ['CS Academic Chair', 'Academic'], ['IS Academic Chair', 'Academic'],
-['Alumni Chair', 'Alumni'], ['Floater', 'Floater'], ['Freshman Representative', 'Freshman Representative'], ['Co-PR Chair', 'Professional Relations']])
+['Alumni Chair', 'Alumni'], ['Floater', 'Floater'], ['Freshman Representative', 'Freshman Representative'],
+['Co-Academic Chair', 'Academic'],['Full Stack Developer','Other'],['Co-PR Chair', 'Professional Relations']])
 
 module.exports = subteamMap;
