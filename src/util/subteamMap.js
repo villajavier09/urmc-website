1025de88ba9f00bd8c67fba4bfb6830c1b1d1cf0
Dezia10 <90987580+Dezia10@users.noreply.github.com>
2022-03-27 15:18:41 -0400

    updated subteammap final

--- src/util/subteamMap.js
+++ src/util/subteamMap.js
@@ -7,7 +7,7 @@ const subteamMap = new Map([['Co-President', 'Presidents'],
 ['Co-Events Chair', 'Events'], ['Co-Outreach Chair', 'Outreach'],
 ['Co-Design Chair', 'Design'], ['Professional Development Chair', 'Professional'],['Professional Development', 'Professional'],
 ['Co-Corporate Chair', 'Corporate'], ['Operations Chair', 'Operations'],['Corporate Chair', 'Corporate'],
-['Secretary', 'Secretary'], ['Co-Mentorship Chair', 'Mentorship'],
+['Secretary', 'Secretary'], ['Co-Mentorship Chair', 'Mentorship'],['Co-Professional Development Chair', 'Professional'],
 ['CS Academic Chair', 'Academic'], ['IS Academic Chair', 'Academic'],
 ['Alumni Chair', 'Alumni'], ['Floater', 'Floater'], ['Freshman Representative', 'Freshman Representative'],
 ['Co-Academic Chair', 'Academic'],['Full Stack Developer','Other'],['Co-PR Chair', 'Professional Relations']])
